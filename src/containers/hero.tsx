6e9838ebcdcb80ed268a3e2a528fd9c965250aed
Clupai8o0 <clupaio4@gmail.com>
2022-09-12 21:00:02 +0300

    changed CV color

--- src/containers/hero.tsx
+++ src/containers/hero.tsx
@@ -16,15 +16,19 @@ function HeroSide() {
 					<p className="tracking-widest">0000 0000 0000 0000</p>
 					{/* User Details */}
 					<div className="flex justify-between w-full mt-2">
-						<p className="text-[10px] tracking-wider opacity-80">Jane Appleseed</p>
+						<p className="text-[10px] tracking-wider opacity-80">
+							Jane Appleseed
+						</p>
 						<p className="text-[10px] tracking-wider opacity-80">00/00</p>
 					</div>
 				</div>
 			</div>
 			<div className="absolute w-64 top-10 right-4">
 				<img src="bg-card-back.png" alt="Credit Card Back" />
-        {/* CV */}
-        <p className="absolute text-white right-8 top-16 text-[8px] opacity-80">000</p>
+				{/* CV */}
+				<p className="absolute text-gray-100 right-8 top-16 text-[8px] opacity-80">
+					000
+				</p>
 			</div>
 		</div>
 	);
